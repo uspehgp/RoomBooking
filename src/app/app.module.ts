aca16c0055626ff15348fd37df1ba9f3184ff215
uspehgp <uspehgp@gmail.com>
2021-06-19 17:27:39 +0300

    add some components, routing, delete "href" in navigate and change in by methods

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { RoomsComponent } from './admin/rooms/rooms.component';
+import { UsersComponent } from './admin/users/users.component';
+import {RouterModule, Routes} from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path : 'admin/users', component : UsersComponent},
+  {path : 'admin/rooms', component: RoomsComponent},
+  {path : '', component: CalendarComponent},
+  {path : '404', component : PageNotFoundComponent},
+  {path : '**', redirectTo : '/404'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CalendarComponent,
+    RoomsComponent,
+    UsersComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
